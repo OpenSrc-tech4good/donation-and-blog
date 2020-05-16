b62455e04ca244f4d4d34f6272631fd8cfe6f640
Zane <zanefinner@pop-os.localdomain>
2020-05-16 11:08:06 -0500

    landing page and site controller create

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,85 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project_name</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="navbar">
+        <div class="left-side"><a href="/">Project_name</a></div>
+        <div class="right-side">
+            <a href="#cause">Our Cause</a>
+            <a href="#donation">Your Donation</a>
+            <a href="#progress">Progress</a>
+            <a href="/blog">Progress</a>
+            <a href="#contact">Contact</a>
+        </div>
+    </div>
+    <div class="jumbotron text-center">
+        <div class="container">
+            <h2 id="cause">Our Cause</h2>
+            <hr>
+            <p>Write what the problem you are adressing is and how it can be solved. This should lead up to an almost call-to-action-esqe link that leads to your funding site</p>
+            <button href="#">Donate Today
+            </button>
+        </div>
+    </div>
+    <div class="container">
+        <h1 class="text-center b-space" id="donation">Your Donation</h1>
+        <div class="row b-space">
+            <div class="col-12 col-md-3">
+                <h3>Donation type 1</h1>
+                <br>
+                <p>Quickly describe how one specific type of donation will go to one specific thing.</p>
+            </div>
+            <div class="col-12 col-md-3">
+                <h3>Donation type 2</h1>
+                <br>
+                <p>Quickly describe how one specific type of donation will go to one specific thing.</p>
+            </div>
+            <div class="col-12 col-md-6">
+                <h3>Any amount Helps</h1>
+                <p>
+                    Explain in a short-but-sweet way how a small donation can benifit your cause.
+                </p>
             </div>
         </div>
-    </body>
+    </div>
+    <div class="container">
+        <h1 class="text-center b-space" id="progress">Progress</h1>
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <h2>What We've Done So Far</h2>
+                <p>Explain how much work you've done and in what way (not funded). Examples include nursing, raising awareness, and starting a successful kickstarter page</p>
+    
+            </div>
+            <div class="col-12 col-md-6">
+                <h2>Our Goal</h2>
+                <div class="prog-bar">
+                    <div class="prog text-center"></div>
+                </div>
+                <p class="text-muted">$14,041 / $86,000</p>
+                <br>
+                <p>Describe why exactly your cause requires the amount stated. What will it go toward? Are there any transaction fees?</p>
+            </div>
+        </div>
+    </div>
+    <div class="contact darki">
+        <div class="container">
+            <h3 class="text-center" id="contact">Contact Info</h3>
+            <div class="text-center">
+                <b>Phone</b>: +1 555-123-1234<br>
+                <b>Email</b>: <a href="mailto:mail@site123.com">mail@site123.com</a><br>
+            </div>
+        </div>
+        <div class="footer">
+
+            <p><a href="https://zanefinner.netlify.app" class="text-center">zanefinner</a> &copy 2020</p>
+
+        </div>
+    </div>
+</body>
 </html>
