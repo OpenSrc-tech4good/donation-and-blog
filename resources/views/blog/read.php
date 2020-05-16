9a66d5be4cbdc1f7a6ebfe36db04fbb379ea1d16
Zane <zanefinner@pop-os.localdomain>
2020-05-16 11:24:54 -0500

    small view adjust in blog

---
+++ resources/views/blog/read.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog - insertTitle</title>
+</head>
+<body>
+    Show info of certain post based of URI ID
+</body>
+</html>
