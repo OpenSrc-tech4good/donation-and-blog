65ab201a1c66d90b1b875a0fffc08cfc0cfef86b
Zane <zanefinner@pop-os.localdomain>
2020-05-16 12:25:34 -0500

    Migrations drafted

--- database/migrations/2020_05_16_161111_create_blogs_table.php
+++ database/migrations/2020_05_16_161111_create_blogs_table.php
@@ -15,6 +15,9 @@ class CreateBlogsTable extends Migration
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->longText('content');
             $table->timestamps();
         });
     }
