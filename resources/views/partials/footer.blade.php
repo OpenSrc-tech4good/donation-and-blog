8a7886f44b41f45e659040556518e3dd57712016
Zane <zanefinner@pop-os.localdomain>
2020-05-17 12:58:47 -0400

    test routes to fotoer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -8,6 +8,16 @@
                     <a href="/"<li>Home</li></a>
                 </ul>
             </div>
+            <div class="col-12 col-md-6">
+                <h2>Blog Test Links</h2>
+    
+                    <ul style="list-style: none" class="no-padding-left">
+                        <a href="/blog"<li>Blog Index Page</li></a>
+                        <br>
+                        <a href="/blog/1/"<li>Blog Test Page</li></a>
+                        <br>
+                    </ul>
+                </div>
         </div>
     </div>
 </div>
